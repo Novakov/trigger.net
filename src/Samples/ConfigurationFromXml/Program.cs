0638765921c36ff8b79b692a4b9b4494668c1b73
Matt Stasch <matt.stasch@gmail.com>
2014-03-02 17:18:31 +0100

    Sample for Configuration from XML

---
+++ src/Samples/ConfigurationFromXml/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConfigurationFromXml
+{
+    using System;
+    using Trigger.NET;
+    using Trigger.NET.Configuration;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scheduler = new Scheduler();
+
+            scheduler.ConfigureFromXml(".\\scheduler.config");
+
+            Console.WriteLine("Press any key to exit...");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class WriteDot : IJob
+    {
+        public void Execute()
+        {
+            Console.WriteLine(".");
+        }
+    }
+}
