6166b243329be9c4e0e55ac5d3d27ba4a3cd1146
hash <hash@users.noreply.github.com>
2015-11-23 13:11:59 +0100

    Scheduler implement IDisposable

--- src/Trigger.NET/Scheduler.cs
+++ src/Trigger.NET/Scheduler.cs
@@ -2,13 +2,15 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
-    public class Scheduler : IScheduler
+    public class Scheduler : IScheduler, IDisposable
     {
         private readonly IContainerFactory containerFactory;
         private readonly ILoggerFactory loggerFactory;
 
         private readonly Dictionary<Guid, Worker> jobs;
+        private bool _disposed;
 
         public Scheduler(ILoggerFactory loggerFactory = null, IContainerFactory containerFactory = null)
         {
@@ -21,6 +23,11 @@
 
         public Guid AddJob<TJob>(JobSetup jobSetup)
         {
+            if (this._disposed)
+            {
+                throw new ObjectDisposedException("Scheduler", "Cannot schedule new jobs.");
+            }
+
             if (jobSetup.JobId == Guid.Empty)
             {
                 jobSetup.JobId = Guid.NewGuid();
@@ -43,6 +50,27 @@
 
                 this.jobs.Remove(jobId);
             }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this._disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.jobs.Keys.ToList().ForEach(this.RemoveJob);
+            }
+
+            this._disposed = true;
         }
     }
 }
